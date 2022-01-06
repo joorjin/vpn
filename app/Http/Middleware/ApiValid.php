f50594bc0c422ba3b0126b5f78c8dcf854cec6b7
joorjin <joorjin2@gmail.com>
2022-01-06 13:43:45 +0330

    test

--- app/Http/Middleware/ApiValid.php
+++ app/Http/Middleware/ApiValid.php
@@ -17,7 +17,7 @@ class ApiValid
      */
     public function handle(Request $request, Closure $next)
     {
-        dd(apache_request_headers());
+        dd(apache_request_headers()['API_KEY']);
         if (!isset(apache_request_headers()['API_KEY'])) {
             return Response()->json([
                 "err"=>"You did not send the key",
